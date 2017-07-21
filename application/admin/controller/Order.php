5cd65830bfab687d1a3b4399c944be57f0b3941c
yangxueqin <865722714@qq.com>
2017-07-21 15:42:48 +0800

    fixbug

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -126,7 +126,7 @@ class Order extends BaseController {
     public function pass() {
         $id = input('id');
         $orderLogic = model('Order', 'logic');
-        $status = ['per_status' => 'pass', 'update_at' => time(), 'pay_time' => time(), 'status' => 'pay_success'];
+        $status = ['per_status' => 'pass', 'update_at' => time(), 'pay_time' => time(), 'status' => 'pay_success','is_pay'=>1,'pay_way'=>4];
         $detail = $orderLogic->updateStatus(['id' => $id], $status);
         if ($detail) {
             LogService::write('订单:' . $id, '凭证审核通过');
